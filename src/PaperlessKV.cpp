ed98d5967a795a463bf045ed6947b95f715bd477
Matthias Bungeroth <bungeroth.matthias@gmail.com>
2020-11-08 21:12:43 +0100

    Local lookup/ put and reomote lookup

--- src/PaperlessKV.cpp
+++ src/PaperlessKV.cpp
@@ -3,8 +3,157 @@
 //
 
 #include "PaperlessKV.h"
+#include "Element.h"
 
-void PaperlessKV::put(char *key, size_t key_len, char *value, size_t value_len) {
+int getCommSize(MPI_Comm comm) {
+  int rank_size_;
+  MPI_Comm_size(comm, &rank_size_);
+  return rank_size_;
+}
+
+PaperlessKV::PaperlessKV(std::string id, MPI_Comm comm,
+                         Consistency c = Consistency::RELAXED)
+    : PaperlessKV(id, comm, 0, c) {}
+
+PaperlessKV::PaperlessKV(std::string id, MPI_Comm comm, HashFunction hf,
+                         Consistency c = Consistency::RELAXED)
+    : id_(id),
+      consistency(c),
+      local_(getCommSize(comm)),
+      remote_(getCommSize(comm)),
+      hash_function_(hf),
+      compactor_(&PaperlessKV::compact, this),
+      dispatcher_(&PaperlessKV::dispatch, this),
+      responder_(&PaperlessKV::respond, this),
+      storage_manager_("/tmp/..."),
+      shutdown_(false),
+      get_value_tagger(0,100000000) {
+  MPI_Comm_rank(comm, &rank_);
+  MPI_Comm_size(comm, &rank_size_);
+}
+
+PaperlessKV::~PaperlessKV() { shutdown_ = true; }
+
+void PaperlessKV::compact() {
+  while (!shutdown_) {
+    DUMMY::Handler handler = local_.getDataChunck();
+    storage_manager_.flushToDisk(handler.get());
+    handler.clear();
+  }
+}
+
+void PaperlessKV::dispatch() {
+  while (!shutdown_) {
+    DUMMY::Handler handler = remote_.getDataChunck();
+    // TODO: Dispatch Data.
+    handler.clear();
+  }
+}
+
+void PaperlessKV::respond() {
+  while(!shutdown_) {
+    MPI_Status status;
+    int key_size;
+    MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &status);
+    MPI_Get_count(&status, MPI_INT, &key_size);
+    char* key_buff = static_cast<char*>(std::malloc(key_size));
+    MPI_Recv(key_buff, key_size, MPI_CHAR, status.MPI_SOURCE, status.MPI_TAG,
+             comm, &status);
+
+    // Query for the element
+    Element key(key_buff, key_size);
+    std::optional<Element> el =
+        localGet(key, hash_function_(key.value, key.len));
+    free(key_buff);
+
+    // Send result might be of 0 len if result is empty.
+    if(el) {
+      MPI_Send(el->value, el->len, MPI_CHAR, status.MPI_SOURCE, status.MPI_TAG, comm);
+    } else {
+      MPI_Send(nullptr, 0, MPI_CHAR, status.MPI_SOURCE, status.MPI_TAG, comm);
+    }
+  }
+}
+
+void PaperlessKV::put(Element key, Element value) {
+  Hash hash = hash_function_(key.value, key.len);
+  Owner o = hash % rank_size_;
+  if (o == rank_) {
+    // Local insert.
+    local_.put(key, value, hash, rank_);
+  } else {
+    if (consistency == RELAXED) {
+      remote_.put(key, value, hash, rank_);
+    } else {
+      // TODO:
+      // Directly dispatch data and wait for response.
+    }
+  }
+}
+
+std::optional<Element> PaperlessKV::get(Element key) {
+  Hash hash = hash_function_(key.value, key.len);
+  Owner o = hash % rank_size_;
+  if (o == rank_) {
+    return localGet(key, hash);
+  } else {
+    if (consistency == RELAXED) {
+      return remoteGetRelaxed(key, hash);
+    } else {
+      return remoteGetNow(key, hash);
+    }
+  }
+}
+
+std::optional<Element> PaperlessKV::localGet(Element key, Hash hash) {
+  std::optional<Element> e_cache = local_cache_.get(key);
+  if (e_cache) {
+    return Element::copyElementContent(*e_cache);
+  } else {
+    std::optional<Element> el = local_.get(key, hash, rank_);
+    if (el) {
+      return el;
+    } else {
+      return storage_manager_.readFromDisk(key);
+    }
+  }
+}
+
+std::optional<Element> PaperlessKV::remoteGetRelaxed(Element key, Hash hash) {
+  std::optional<Element> e_cache = remote_cache_.get(key);
+  if (e_cache) {
+    return Element::copyElementContent(*e_cache);
+  } else {
+    std::optional<Element> el = remote_.get(key, hash, rank_);
+    if (el) {
+      return el;
+    } else {
+      return remoteGetNow(key, hash);
+    }
+  }
+}
+
+std::optional<Element> PaperlessKV::remoteGetNow(Element key, Hash hash) {
+  Owner o = hash & rank_size_;
+  int tag = get_value_tagger.getNextTag();
+
+  MPI_Send(key.value, key.len, MPI_CHAR, o, tag, comm);
+
+  MPI_Status status;
+  int value_size;
+  MPI_Probe(o, tag, comm, &status);
+  MPI_Get_count(&status, MPI_INT, &value_size);
+  if(value_size == 0) {
+    MPI_Recv(nullptr, 0, MPI_CHAR, o, tag,comm, &status);
+    return std::nullopt;
+  }
+  char* value_buff = static_cast<char*>(std::malloc(value_size));
+  MPI_Recv(value_buff, value_size, MPI_CHAR, o, tag,comm, &status);
+  return Element(value_buff, value_size);
+}
+
+void PaperlessKV::put(char *key, size_t key_len, char *value,
+                      size_t value_len) {
   put(Element(key, key_len), Element(value, value_len));
 }
 
@@ -15,3 +164,6 @@ void PaperlessKV::get(char *key, size_t key_len) {
 void PaperlessKV::deleteKey(char *key, size_t key_len) {
   deleteKey(Element(key, key_len));
 }
+
+
+
--- src/PaperlessKV.h
+++ src/PaperlessKV.h
@@ -5,27 +5,101 @@
 #ifndef PAPERLESS_PAPERLESSKV_H
 #define PAPERLESS_PAPERLESSKV_H
 
+#include <mpi.h>
 
+#include <atomic>
+#include <condition_variable>
 #include <functional>
-#include <cstddef>
+#include <string>
+#include <thread>
+
 #include "Element.h"
+#include "MemoryTableManager.h"
+#include "StorageManager.h"
+#include "Types.h"
+
+class DUMMY {
+ public:
+  class Handler {
+   public:
+    void clear();
+    MemoryTable get();
+  };
+};
 
 class PaperlessKV {
-public:
-  explicit PaperlessKV(int id);
-  PaperlessKV(int id, std::function<uint64_t(char*,size_t)>);
+ public:
+  enum Consistency { SEQUENTIAL, RELAXED };
+
+  explicit PaperlessKV(std::string id, MPI_Comm comm, Consistency);
+  PaperlessKV(std::string id, MPI_Comm comm, HashFunction, Consistency);
 
   PaperlessKV(const PaperlessKV&) = delete;
   PaperlessKV(PaperlessKV&&);
+
+  ~PaperlessKV();
 
   void put(char* key, size_t key_len, char* value, size_t value_len);
   void get(char* key, size_t key_len);
   void deleteKey(char* key, size_t key_len);
 
   void put(Element key, Element value);
-  void get(Element key);
+  std::optional<Element> get(Element key);
   void deleteKey(Element key);
+
+ private:
+
+  void compact();
+  void dispatch();
+  void respond();
+
+  std::optional<Element>  localGet(Element key, Hash hash);
+
+  std::optional<Element> remoteGetRelaxed(Element key, Hash hash);
+  std::optional<Element> remoteGetNow(Element key, Hash hash);
+
+  std::string id_;
+
+  Consistency consistency;
+  HashFunction hash_function_;
+
+  MemoryTableManager<DUMMY> local_;
+  MemoryTableManager<DUMMY> remote_;
+
+  MemoryTable local_cache_;
+  MemoryTable remote_cache_;
+
+  std::thread compactor_;
+  std::thread dispatcher_;
+  std::thread responder_;
+
+  StorageManager storage_manager_;
+
+  std::atomic<bool> shutdown_;
+
+  int rank_;
+  int rank_size_;
+  MPI_Comm comm;
+
+
+  class Tagger {
+   public:
+    Tagger(int min, int max)
+      : max_get_key(max), min_get_key(min) {
+    }
+    int getNextTag() {
+      return  min_get_key + (cur_get_key++ % max_get_key);
+    }
+   private:
+    const int max_get_key;
+    const int min_get_key;
+    std::atomic<int> cur_get_key;
+  };
+
+  Tagger get_value_tagger;
+
+
+
 };
 
-
-#endif //PAPERLESS_PAPERLESSKV_H
+#endif  // PAPERLESS_PAPERLESSKV_H
