d4d2d51683b15761a43bf23cd27c54ba1549e098
Matthias Bungeroth <bungeroth.matthias@gmail.com>
2020-11-08 17:47:07 +0100

    Iterface change

--- src/MemoryTable.h
+++ src/MemoryTable.h
@@ -8,6 +8,7 @@
 #include <cstddef>
 #include <functional>
 #include "Element.h"
+#include "Status.h"
 
 // Should this be a pure virtual class? So we can extend it as a RBTree Memory table?
 // We could also not make this virtual and just manually enforce the interface this is better for performance!
@@ -19,7 +20,7 @@ public:
 
   // Whats about the tombstone bit?=
   void put(Element key, Element val);
-  Element get(Element key);
+  StatusOr<Element> get(Element key);
 
 
   // Required for flushing to disk in the StorageManager
