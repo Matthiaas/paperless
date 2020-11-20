4c9c6ee2c962803da89f6452cbfd83843c4ca16b
floris <westermann.floris@gmail.com>
2020-11-20 15:02:13 +0100

    Set cpp standard to 20 and remove accidentally pushed stuff.

--- main.cpp
+++ main.cpp
@@ -2,25 +2,6 @@
 #include "src/StorageManager.h"
 
 int main() {
-  StorageManager storage_manager{"/tmp/paperless"};
-  RBTreeMemoryTable table1{};
-  RBTreeMemoryTable table2{};
-
-  char key[] = "key thingy";
-  char value[] = "value thingy";
-
-  table1.put({key, sizeof key}, {value, sizeof value});
-  storage_manager.flushToDisk(table1);
-
-//  table2.put({key, sizeof key}, Tomblement::getATombstone());
-//  storage_manager.flushToDisk(table2);
-
-  auto res = storage_manager.readFromDisk({key, sizeof key});
-
-  if (res.hasValue()) {
-    std::cout << res.Value().Value() << std::endl;
-  } else {
-    std::cout << res.Status() << std::endl;
-  }
+  std::cout << "Welcome to Paperless\n";
   return 0;
 }
