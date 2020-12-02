1bbef769ac93b0ac36dd570ef3a7c360aa2cc727
Matthias Bungeroth <bungeroth.matthias@gmail.com>
2020-12-03 00:00:56 +0100

    Add benchmark file

--- benchmark.sh
+++ benchmark.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 # Call with argument basic or workload
 KEYLEN=16
-VALLEN=(8, 64, 512)
-COUNT=1000
-RANKS=(1 2 4 8) # 16 20 40 80 160 320)
+VALLEN=(8) #, 64, 512)
+COUNT=10000
+RANKS=(1 2 4)  #8 16 20 40 80 160 320)
 
 
 for i in "${RANKS[@]}"; do
     for j in "${VALLEN[@]}"; do
-
-        for ((j=0; j <$i; j++))
-        do
-          mkdir /tmp/mydb${j}
-        done
-        mpirun -np $i ./build/$1 $KEYLEN $j $COUNT 50
+        mpirun -np $i ./build/$1 $KEYLEN $j $COUNT 0
         for ((j=0; j <$i; j++))
         do
           rm -r /tmp/mydb${j}
