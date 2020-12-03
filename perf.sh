09f513994f1202a0a3a93e5d6cf8a37fab8d496c
Matthias Bungeroth <mbungeroth@eu-login-46.euler.ethz.ch>
2020-12-03 23:03:58 +0100

    Add perf script

---
+++ perf.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#BSUB -P paperless
+#BSUB -J workload
+#BSUB -o output.o%J
+#BSUB -W 02:00
+#BSUB -n 20
+
+# On cluster run: bsub < benchmark_cluster.sh
+
+# Call with argument basic or workload
+KEYLEN=16
+VALLEN=(8) #, 64, 512)
+COUNT=10000
+RANKS=(1 2 4 8) # 16 20) # 40 80 160 320)
+
+
+export MAX_LOCAL_MEMTABLE_SIZ=100000
+export MAX_REMOTE_MEMTABLE_SIZE=100000
+export MAX_LOCAL_CACHE_SIZE=100000
+export MAX_REMOTE_CACHE_SIZE=100000
+export DISPATCH_IN_CHUNKS=1
+export STORAGE_LOCATION=/scratch/mydb
+
+
+
+
+for i in "${RANKS[@]}"; do
+    for j in "${VALLEN[@]}"; do
+        perf record -g  mpirun -np $i ./build/workload $KEYLEN $j $COUNT 0
+    done
+done
