3ef8ee328f734b93eb3544b3ccd1cda03786ec7b
Matthias Bungeroth <mbungeroth@eu-login-15.euler.ethz.ch>
2020-12-10 03:47:14 +0100

    Fix

--- benchmark.sh
+++ benchmark.sh
@@ -15,18 +15,18 @@ UPDATE_RATIO=0
 RANKS=(8) #8 16 20) # 40 80 160 320)
 
 
-export MAX_LOCAL_MEMTABLE_SIZE=1000000
-export MAX_REMOTE_MEMTABLE_SIZE=1000000
-export MAX_LOCAL_CACHE_SIZE=1000000
-export MAX_REMOTE_CACHE_SIZE=1000000
+export MAX_LOCAL_MEMTABLE_SIZE=10000000
+export MAX_REMOTE_MEMTABLE_SIZE=10000000
+export MAX_LOCAL_CACHE_SIZE=10000000
+export MAX_REMOTE_CACHE_SIZE=10000000
 export DISPATCH_IN_CHUNKS=1
-export STORAGE_LOCATION=/tmp/mydb
+export STORAGE_LOCATION=/scratch/mydb
 
 
 
 
 for i in "${RANKS[@]}"; do
     for j in "${VALLEN[@]}"; do
-        mpirun -np $i ./build/thegreatebenchmark $KEYLEN $j $COUNT $UPDATE_RATIO /home/matthias/Documents/eth/dphpc/paperless/analytics/data/localRun/8/
+       mpirun -np $i ./build/thegreatbenchmark $KEYLEN $j $COUNT $UPDATE_RATIO /cluster/scratch/$USER/512MB/$i
     done
 done
