c873fa001152ebebd3b72dfd9f1709fce67d36a4
Matthias Bungeroth <bungeroth.matthias@gmail.com>
2020-12-10 02:34:16 +0100

    Change to python 3

--- analytics/runNotebook.sh
+++ analytics/runNotebook.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export PAPERLESS_KV_DATA_DIR=$PWD"/""$(dirname "$BASH_SOURCE")"
-ipython notebook $PAPERLESS_KV_DATA_DIR/KV_plots.ipynb
+ipython3 notebook $PAPERLESS_KV_DATA_DIR/KV_plots.ipynb
--- analytics/setupNotebook.sh
+++ analytics/setupNotebook.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sudo apt install jupyter -y
-pip3 install jupytext
+sudo pip3 install jupytext
 jupyter serverextension enable jupytext
 jupytext --set-formats ipynb,py KV_plots.py
