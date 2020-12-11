3f3bbf6f0856fd39d95a9e7eebc97f357fce989e
sobkulir <r.sobkuliak@gmail.com>
2020-12-11 11:34:45 +0100

    Add results from paperless/papyrus comparison.

--- analytics/runNotebook.sh
+++ analytics/runNotebook.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This is a magic line from SO:
+# 	https://stackoverflow.com/a/58081608
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 conda activate paperless
 export PAPERLESS_KV_DATA_DIR=$PWD"/""$(dirname "$BASH_SOURCE")"
 
--- analytics/setupNotebook.sh
+++ analytics/setupNotebook.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 conda env create -f environment.yml
+# This is a magic line from SO:
+# 	https://stackoverflow.com/a/58081608
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+conda activate paperless
+
 jupyter serverextension enable jupytext
 jupytext --set-formats ipynb,py KV_plots.py
