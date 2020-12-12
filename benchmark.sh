d373b4110504dccd3aa3a6900d72b922686f7162
Matthias Bungeroth <mbungeroth@eu-login-33.euler.ethz.ch>
2020-12-12 20:16:50 +0100

    for cluster

--- benchmark.sh
+++ benchmark.sh
@@ -12,7 +12,7 @@ KEYLEN=16
 VALLEN=(8) #, 64, 512)
 COUNT=10000
 UPDATE_RATIO=0
-RANKS=(16) #8 16 20) # 40 80 160 320)
+RANKS=(4, 8, 324, 8, 324, 8, 324, 8, 32) #8 16 20) # 40 80 160 320)
 
 
 export MAX_LOCAL_MEMTABLE_SIZE=10000000
@@ -20,14 +20,14 @@ export MAX_REMOTE_MEMTABLE_SIZE=10000000
 export MAX_LOCAL_CACHE_SIZE=10000000
 export MAX_REMOTE_CACHE_SIZE=10000000
 export DISPATCH_IN_CHUNKS=1
-export STORAGE_LOCATION=/tmp/mydb
+export STORAGE_LOCATION=/scratch/mydb
 
 
 
 
 for i in "${RANKS[@]}"; do
     for j in "${VALLEN[@]}"; do
-       mpirun --use-hwthread-cpus -np $i ./build/thegreatbenchmark_paperless $KEYLEN $j $COUNT $UPDATE_RATIO /home/matthias/Documents/eth/dphpc/paperless/analytics/data/localRunIprobe/$i
+       mpirun -np $i ./build/thegreatbenchmark_paperless $KEYLEN $j $COUNT $UPDATE_RATIO /cluster/scratch/$USER/IprobeExperimentEuler/paperless/$i
        #mpirun -np $i ./build/thegreatbenchmark_papyrus $KEYLEN $j $COUNT $UPDATE_RATIO /cluster/scratch/$USER/512MB/papyrus/$i
     done
 done
