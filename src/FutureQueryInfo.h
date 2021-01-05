e192ab7905628395c3b5a5fdd489f92030587f93
Matthias Bungeroth <bungeroth.matthias@gmail.com>
2021-01-05 01:22:24 +0100

    Speedup Remote dispatch by using async comm and fix Segfaults on Iget

--- src/FutureQueryInfo.h
+++ src/FutureQueryInfo.h
@@ -7,14 +7,21 @@
 
 #include "Message.h"
 #include "Status.h"
+#include <mpi.h>
 
 class FutureQueryInfo {
  public:
-  FutureQueryInfo() {
-    has_value_ = false;
+  FutureQueryInfo(size_t tag, ElementView key, Hash hash) :
+      send_msg_(Message::GET_REQUEST),
+      key_(key.Value(), key.Length())  {
     requests_ = static_cast<MPI_Request*>(malloc(4 * sizeof (MPI_Request)));
+    has_value_ = false;
+
+    send_msg_.SetTag(tag);
+    send_msg_.SetKeyLen(key.Length());
+    send_msg_.SetHash(hash);
   }
-  FutureQueryInfo(std::pair<QueryStatus, size_t> result) {
+  FutureQueryInfo(std::pair<QueryStatus, size_t> result) : key_(nullptr, 0) {
     has_value_ = true;
     result_ = result;
     requests_ = nullptr;
@@ -23,21 +30,25 @@ class FutureQueryInfo {
   FutureQueryInfo(const FutureQueryInfo&) = delete;
   FutureQueryInfo& operator=(const FutureQueryInfo&) = delete;
 
-  FutureQueryInfo(FutureQueryInfo&& other) {
-    requests_ = other.requests_;
+  FutureQueryInfo(FutureQueryInfo&& other) :
+      requests_(other.requests_),
+      status_(other.status_),
+      msg_(std::move(other.msg_)),
+      send_msg_(std::move(other.send_msg_)),
+      key_(std::move(other.key_)),
+      has_value_(other.has_value_),
+    result_(std::move(other.result_)){
     other.requests_ = nullptr;
-    status = other.status;
-    msg_ = std::move(other.msg_);
-    result_ = other.result_;
-    has_value_ = other.has_value_;
   }
   FutureQueryInfo& operator=(FutureQueryInfo&& other) {
     requests_ = other.requests_;
     other.requests_ = nullptr;
-    status = other.status;
+    status_ = other.status_;
     msg_ = std::move(other.msg_);
-    result_ = other.result_;
+    send_msg_ = std::move(other.send_msg_);
+    key_ = std::move(other.key_);
     has_value_ = other.has_value_;
+    result_ = other.result_;
     return *this;
   }
 
@@ -69,10 +80,20 @@ class FutureQueryInfo {
   // 3 is MSG Recv
   // 4 is VALUE Recv
   MPI_Request* requests_;
-  MPI_Status status;
+  MPI_Status status_;
   Message msg_;
+  Message send_msg_;
+
+  Element key_;
   int has_value_;
   std::pair<QueryStatus, size_t> result_;
+
+  void InitRequest(Owner o, const ElementView &v_buff, MPI_Comm comm) {
+    send_msg_.I_SendMessage(o, PAPERLESS_MSG_TAG, comm, &requests_[0]);
+    MPI_Isend(key_.Value(), key_.Length(), MPI_CHAR, o, send_msg_.GetTag(), comm, &requests_[1]);
+    msg_.I_ReceiveMessage(o, send_msg_.GetTag(), comm, &requests_[2]);
+    MPI_Irecv(v_buff.Value(), v_buff.Length(), MPI_CHAR, o, send_msg_.GetTag(), comm, &requests_[3]);
+  }
 
   void Destruct() {
     free(requests_);
