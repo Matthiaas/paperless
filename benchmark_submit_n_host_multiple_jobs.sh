da56e7e9f2deaf2a2fc44d4db02f4842ee5d4186
Matthias Bungeroth <bungeroth.matthias@gmail.com>
2021-01-14 00:26:49 +0100

    fix

--- benchmark_submit_n_host_multiple_jobs.sh
+++ benchmark_submit_n_host_multiple_jobs.sh
@@ -1,5 +1,5 @@
 for k in $(seq 30); do
-  sed "s/KV="KV"/KV="papyrus"/g; s/report_one_host/report_n_host/g; s/st=0/st=${k}/g" benchmark_throughput_only_one.sh | bsub -R "rusage[scratch=5120] span[ptile=2] select[model==EPYC_7742]"
-  sed "s/KV="KV"/KV="paperless"/g; s/report_one_host/report_n_host/g; s/st=0/st=${k}/g" benchmark_throughput_only_one.sh | bsub -R "rusage[scratch=5120] span[ptile=2] select[model==EPYC_7742]"
-  sed "s/KV="KV"/KV="Ipaperless"/g; s/report_one_host/report_n_host/g; s/st=0/st=${k}/g" benchmark_throughput_only_one.sh | bsub -R "rusage[scratch=5120] span[ptile=2] select[model==EPYC_7742]"
+  sed "s/KV=\"KV\"/KV=\"papyrus\"/g; s/report_one_host/report_n_host/g; s/st=0/st=${k}/g" benchmark_throughput_only_one.sh | bsub -R "rusage[scratch=5120] span[ptile=2] select[model==EPYC_7742]"
+  sed "s/KV=\"KV\"/KV=\"paperless\"/g; s/report_one_host/report_n_host/g; s/st=0/st=${k}/g" benchmark_throughput_only_one.sh | bsub -R "rusage[scratch=5120] span[ptile=2] select[model==EPYC_7742]"
+  sed "s/KV=\"KV\"/KV=\"Ipaperless\"/g; s/report_one_host/report_n_host/g; s/st=0/st=${k}/g" benchmark_throughput_only_one.sh | bsub -R "rusage[scratch=5120] span[ptile=2] select[model==EPYC_7742]"
 done
